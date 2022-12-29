55d57d8ba7966a4d3d0e2ac7d5a9c04069ee23b1
Salman Nisar <salman.nisar@nagarro.com>
2022-12-29 10:53:32 +0400

    on list image display

--- lib/application/screens/contact_list/widgets/contact_list_item.dart
+++ lib/application/screens/contact_list/widgets/contact_list_item.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:contact_app_bloc_architecture/data/model/contact_data_model.dart';
 import 'package:flutter/material.dart';
 
@@ -14,8 +16,15 @@ class ContactListItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
       child: ListTile(
-        leading: const CircleAvatar(
-          child: Text("Lead"),
+        leading: AspectRatio(
+          aspectRatio: 1,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Image.file(
+              File(contactDataModel.profilePicture),
+              fit: BoxFit.fill,
+            ),
+          ),
         ),
         title: Text(contactDataModel.name),
         subtitle: Text(contactDataModel.mobileNumber),
